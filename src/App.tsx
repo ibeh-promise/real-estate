8df4568026ac4626cf82d5d97b9c0730c6763219
Promise Ibeh <118574387+ibeh-promise@users.noreply.github.com>
2025-09-09 04:16:23 +0000

    removed Link at app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';import './App.css'
 // import * as motion from "motion/react-client"
 import Home from './pages/Home';
 
